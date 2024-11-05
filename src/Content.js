d551c50a3863e5f528cf5af849a464320547b42c
eliyasmd50 <eliyasmohamed50@gmail.com>
2024-11-06 00:26:36 +0530

    click events

--- src/Content.js
+++ src/Content.js
@@ -4,12 +4,25 @@ const Content = () => {
         const int = Math.floor(Math.random() * name.length);
         return name[int];
       }
-      
+    
+    const handleClick = () => {
+        console.log("you clicked it");
+    }
+    const handleClick2 = (name) => {
+        console.log(`${name} clicked it`)
+    }
+    const handleClick3 = (e) => {
+        console.log(e.target.innerText)
+    }
+
   return (
     <main>
-        <p>
+        <p onDoubleClick={handleClick}>
             Hello {handleChange()}!
         </p>
+        <button onClick={handleClick}>Click It</button>
+        <button onClick={() => handleClick2('Eliyas')}>Click It</button>
+        <button onClick={(e) => handleClick3(e)}>Click It</button>
     </main>
   )
 }
